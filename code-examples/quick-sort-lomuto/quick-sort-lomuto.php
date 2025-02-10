47bc0c0cf125ac146424dc20e504e8a9b4a01367
aspirantzhang <admin@aspirantzhang.com>
2025-02-10 13:31:06 +0800

    feat: add PHP implementation and documentation for Lomuto partition quick sort

---
+++ code-examples/quick-sort-lomuto/quick-sort-lomuto.php
@@ -0,0 +1,43 @@
+<?php
+// TODO: FORMAT DOCUMENT
+class Solution {
+    public function sortArray($nums) {
+        $this->sortIterative($nums);
+        return $nums;
+    }
+
+    private function sortIterative(&$nums)
+    {
+        $stack = [[0, count($nums) -1]];
+        while(!empty($stack)){
+            [$lowMin, $highMax] = array_pop($stack);
+
+            if($lowMin >= $highMax) { continue; }
+
+            $pivotIndex = $this->partition($nums,$lowMin,$highMax);
+            
+            array_push($stack, [$pivotIndex + 1, $highMax]);
+            array_push($stack, [$lowMin ,$pivotIndex - 1]);
+        }
+    }
+
+    private function partition(&$nums, $lowMin, $highMax)
+    {
+        $randomIndex = rand($lowMin, $highMax);
+        [$nums[$randomIndex], $nums[$highMax]] = [$nums[$highMax],$nums[$randomIndex]];
+
+        $pivot = $nums[$highMax];
+        $lowMax = $lowMin-1;
+
+        for ($i = $lowMin; $i < $highMax; $i++) {
+            if($nums[$i] < $pivot) {
+                $lowMax++;
+                [$nums[$i], $nums[$lowMax]] = [$nums[$lowMax],$nums[$i]];
+            }
+        }
+
+        [$nums[$lowMax + 1],$nums[$highMax]] = [$nums[$highMax],$nums[$lowMax + 1]];
+
+        return $lowMax + 1;
+    }
+}
